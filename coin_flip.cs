60873a659ecd16ce2d8b00aebd074d37986f6e1e
Aishwarya Rahatal <aishwarya.rahatal97@gmail.com>
2021-09-15 14:47:40 +0530

    coin flip code

---
+++ coin_flip.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace coin_flip
+{
+    class coin_flip
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Coin flip program :");
+
+            Console.WriteLine("Flipping coin...");
+
+            Random random = new Random();
+
+            int value = random.Next(0, 1);
+
+            if(value==1)
+            {
+                Console.WriteLine("You got Heads!!");
+            }
+            else
+            {
+                Console.WriteLine("You got Tails!!");
+
+            }
+
+        }
+    }
+}
