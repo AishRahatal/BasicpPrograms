0cf516d63639cd78cceac86b68786572b944a24c
Aishwarya Rahatal <aishwarya.rahatal97@gmail.com>
2021-09-15 14:48:49 +0530

    leap year code

---
+++ leap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace leap_year
+{
+    class leap
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Leap year program :");
+
+            Console.WriteLine("Enter year :");
+            int year= Convert.ToInt32(Console.ReadLine());
+            float rm1 = year % 4;
+            float rm2 = year % 400;
+            if ( rm1== 0 || rm2==0)
+            {
+                Console.WriteLine(year + "\tis a Leap year");
+
+            }
+            else
+            {
+                Console.WriteLine(year + "\tis not a Leap year");
+            }
+                    
+        }
+    }
+}
