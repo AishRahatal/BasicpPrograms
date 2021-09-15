3808a45fa61344327b08ebd8ecf35f0e26e97ee4
Aishwarya Rahatal <aishwarya.rahatal97@gmail.com>
2021-09-15 14:51:30 +0530

    calculating prime factors

---
+++ Prime.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace primefactors
+{
+    class Prime
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prime factors program");
+            Console.WriteLine("Enter number:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] prime = { 0 };
+            Console.WriteLine("Prime factors of :" + n + "\tare :");
+            for (int i = 2; i <= n; i++)
+            {
+                while (n % i == 0)
+                {
+                    Console.WriteLine(i);
+
+                    n = n / i;
+                }
+            }
+
+        }
+
+    }
+}
