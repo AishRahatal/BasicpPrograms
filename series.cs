db176e46a26cf2133a00b4e005464b7ab9538aab
Aishwarya Rahatal <aishwarya.rahatal97@gmail.com>
2021-09-15 14:49:34 +0530

    printing harmonic series code

---
+++ series.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace harmonicseries
+{
+    class series
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Harmonic series Program");
+            
+            Console.WriteLine("Enter number to get harmonic series:");
+            int n = Convert.ToInt32(Console.ReadLine());
+          
+            Console.WriteLine("H(n)= ");
+            for ( int i=1; i<=n; i++)
+            {
+                Console.WriteLine("1/" + i);
+                if (i != n)
+                Console.WriteLine("+");
+
+
+            }
+        }
+
+     
+    }
+}
