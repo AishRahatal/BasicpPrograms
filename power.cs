77d94f109cdbc8edfe225f415eedd7311f41dcc2
Aishwarya Rahatal <aishwarya.rahatal97@gmail.com>
2021-09-15 14:50:25 +0530

    calculating power of 2

---
+++ power.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace powerof2
+{
+    class power
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculate power of 2 program");
+
+            Console.WriteLine("Enter number:");
+            int n = Convert.ToInt32(Console.ReadLine());
+          
+
+            if(n<32)
+            {
+
+                Console.WriteLine(n.ToString() +"to power of 2" + "=" + Math.Pow(2, n));
+            }
+
+
+        }
+    }
+}
