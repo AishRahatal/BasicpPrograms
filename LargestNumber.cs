178197420e4c44daaee7943bb7c084369e8c0eb7
Aishwarya Rahatal <aishwarya.rahatal97@gmail.com>
2021-09-16 14:48:15 +0530

    code to get large number

---
+++ LargestNumber.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Largestnumber
+{
+    class LargestNumber
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program to Find the Largest Among Three Numbers");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Enter first number:");
+            int fnum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            int snum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter third number:");
+            int tnum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("-------------------------------------");
+           //int large;
+            if (fnum > snum && fnum > tnum)
+            { 
+                Console.WriteLine("Largest NUmber = " + fnum);
+
+            }
+            else if (snum > fnum && snum > tnum)
+                {
+             
+                    Console.WriteLine("Largest NUmber = " +  snum);
+
+                }
+                else if (tnum > fnum && tnum > snum)
+                    {
+                      
+                        Console.WriteLine("Largest NUmber = " + tnum);
+
+                 }
+                
+
+
+        }
+
+
+    }
+}
+
