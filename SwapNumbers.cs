88b62bda780ac08aa87f794735569d10845d0b4c
Aishwarya Rahatal <aishwarya.rahatal97@gmail.com>
2021-09-16 14:49:00 +0530

    code for swapping of two numbers

---
+++ SwapNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SwapNumbers
+{
+    class SwapNumbers
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program to Swap Two Numbers");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Enter first number:");
+            int first_num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            int second_num = Convert.ToInt32(Console.ReadLine());
+            int temp;
+            Console.WriteLine("-------------------------------------");
+
+            Console.WriteLine("Numbers before Swap : first number="+first_num +"\tsecond number=" +second_num);
+            Console.WriteLine("---------------------------------------------------------------------------");
+
+            temp = first_num;
+            first_num = second_num;
+            second_num = temp;
+            
+            Console.WriteLine("Numbers before Swap : first number=" + first_num +"\tsecond number=" + second_num);
+
+
+        }
+    }
+}
