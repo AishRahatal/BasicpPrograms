88dbca5590c47edcab671a6fcb7d8f152ac11bba
Aishwarya Rahatal <aishwarya.rahatal97@gmail.com>
2021-09-16 14:34:03 +0530

    calculating quotient and remainder

---
+++ Quotient_Remainder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Quotientandremainder
+{
+    class Quotient_Remainder
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program to Compute Quotient and Remainder");
+            Console.WriteLine("-------------------------------------");
+            float quotient, remainder;
+            Console.WriteLine("Enter divident:");
+            int dividend = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter divisor:");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+            quotient = dividend / divisor;
+            remainder = dividend % divisor;
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Dividend:{0} Divisor:{1}", dividend, divisor);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+            Console.ReadLine();
+        }
+    }
+}
