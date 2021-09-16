4e72a6533d325e51933a24b156e10aadfd474c67
Aishwarya Rahatal <aishwarya.rahatal97@gmail.com>
2021-09-16 14:47:10 +0530

    checking charater is vowel or consonant

---
+++ VowelsConsonant.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace alphabets
+{
+    class VowelsConsonant
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program to Check Whether an Alphabet is Vowel or Consonant");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Enter an alphabet:");
+            char alphabet = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("-------------------------------------");
+
+            if ( alphabet=='a' || alphabet=='e'|| alphabet=='i'|| alphabet=='o'|| alphabet=='u'||
+               alphabet == 'A' || alphabet == 'E' || alphabet == 'I' || alphabet == 'O' || alphabet == 'U')
+            {
+                Console.WriteLine("An alphabet is a Vowel ");
+
+            }
+            else
+            {
+                Console.WriteLine("An alphabet is a Consonant ");
+
+            }
+        }
+    }
+}
